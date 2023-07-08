2f9e42234627c1caf0c46a957413cf3338a8f0c3
MatszTheNerd <matszagorski@gmail.com>
2023-07-08 14:50:21 +0100

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 let randomNum = Math.floor(Math.random() * 20 + 1);
-console.log(randomNum);
 let score = 15;
+const reset = document.querySelector(".top .again");
 scoreNum = document.querySelector(".scorenum");
+console.log(randomNum);
+let qMark = document.querySelector(".qmark");
 
+reset.addEventListener("click", () => {
+  console.log("good");
+  scoreNum.textContent = 15;
+  document.querySelector(".number").value = 0;
+  document.querySelector(".startguess").textContent = "Start guessing...";
+});
 document.querySelector(".check").addEventListener("click", function () {
   let inputNumber = Number(document.querySelector(".number").value);
 
   if (randomNum === inputNumber) {
     document.querySelector(".startguess").textContent = "Correct!";
     score++;
+    qMark.textContent = "✓";
     scoreNum.textContent = score;
+    document.querySelector("body").classList.add("#");
   } else if (randomNum < inputNumber) {
     if (score >= 1)
       document.querySelector(".startguess").textContent = "Too high";
